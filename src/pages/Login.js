20e913297fc13f09e8c2e50b11c9ec817e36d4b3
Bruno <brunohgrau99@gmail.com>
2024-03-22 04:04:58 -0300

    Register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,13 +5,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import Step from "@mui/material/Step";
-import StepLabel from "@mui/material/StepLabel";
-import Stepper from "@mui/material/Stepper";
-import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
-
-const steps = ["Shipping", "Payment", "Order"];
 
 const Login = () => {
   return (
@@ -72,7 +66,7 @@ const Login = () => {
       </Box>
       <Box sx={{ display: "flex", alignItems: "center" }}>
         <Typography variant="body2">New Customer?</Typography>
-        <Link href="#" variant="body2" sx={{ ml: 2 }}>
+        <Link href="/register" variant="body2" sx={{ ml: 2 }}>
           Register
         </Link>
       </Box>
